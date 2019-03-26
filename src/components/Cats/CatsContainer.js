705e7c251919d943a72359836c4a2c8098ad8c53
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-03-26 21:06:58 +0100

    :sparkles: Add drag and drop grid

--- src/components/Cats/CatsContainer.js
+++ src/components/Cats/CatsContainer.js
@@ -2,5 +2,8 @@ import { connect } from 'react-redux'
 
 import { Cats } from './Cats'
 
-export const CatsContainer = connect(state => ({ cats: state.cats }))(Cats)
+export const CatsContainer = connect(
+  state => ({ cats: state.cats }),
+  dispatch => ({ moveCat: (source, target) => dispatch({ type: 'MOVE_CAT', source, target }) })
+)(Cats)
 
