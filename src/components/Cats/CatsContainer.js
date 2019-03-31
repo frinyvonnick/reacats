bea417550e5b5b2d8de73bcd29241266edbda577
Benjamin Cavy <benjamin.cavy@gmail.com>
2019-03-31 23:43:31 +0200

    Store cats in localStorage

--- src/components/Cats/CatsContainer.js
+++ src/components/Cats/CatsContainer.js
@@ -1,10 +1,11 @@
 import { connect } from "react-redux";
 
-import { Cats } from "./Cats";
+import Cats from "./Cats";
 
 export const CatsContainer = connect(
   state => ({ cats: state.cats }),
   dispatch => ({
-    moveCat: (source, target) => dispatch({ type: "MOVE_CAT", source, target })
+    moveCat: (source, target) => dispatch({ type: "MOVE_CAT", source, target }),
+    readCats: cats => dispatch({ type: "SET_CATS", cats })
   })
 )(Cats);
