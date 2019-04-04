9eb68aeaa1513baaab70723a31946f5d3a12c25a
Benjamin Cavy <benjamin.cavy@gmail.com>
2019-04-04 23:58:40 +0200

    Form refactoring using naïve hooks replacements

--- src/components/SearchCat/SearchCatForm.jsx
+++ src/components/SearchCat/SearchCatForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import Select from "react-select";
 import "./SearchCatForm.css";
 import config from "../../config.json";
@@ -8,139 +8,101 @@ const imageTypes = ["jpg", "png", "gif"].map(v => ({ value: v, label: v }));
 const apiKey = config.apiKey;
 const baseUrl = "https://api.thecatapi.com/v1";
 
-class SearchCatForm extends Component {
-  constructor(props) {
-    super(props);
+function SearchCatForm({ excludedCats, addCats }) {
+  const [cats, setCats] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedCats, setSelectedCats] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState();
+  const [selectedImageTypes, setSelectedTimageTypes] = useState([]);
 
-    this.searchCat = this.searchCat.bind(this);
-    this.toggle = this.toggle.bind(this);
-
-    this.state = {
-      cats: [],
-      allCategories: [],
-      loadingCats: false,
-      selectedCats: [],
-      selectedCategory: null,
-      selectedImageTypes: [],
-      allImageTypes: imageTypes
-    };
-
+  useEffect(() => {
     fetch(`${baseUrl}/categories`)
       .then(response => response.json())
       .then(allCategories =>
         allCategories.map(({ name, id }) => ({ value: id, label: name }))
       )
-      .then(allCategories => this.setState({ allCategories }));
+      .then(setCategories);
+  }, [setCategories]);
 
-    fetch(`${baseUrl}/breeds`)
-      .then(response => response.json())
-      .then(allBreeds =>
-        allBreeds.map(({ name, id }) => ({ value: id, label: name }))
-      )
-      .then(allBreeds => this.setState({ allBreeds }));
-  }
-
-  componentDidMount() {
-    this.searchCat();
-  }
-
-  searchCat() {
-    const { selectedImageTypes, selectedCategory } = this.state;
+  useEffect(() => {
+    // FIXME store values ?
     const imageTypes = selectedImageTypes.map(t => t.value);
     const headers = new Headers();
     headers.append("x-api-key", apiKey);
-    this.setState({ loadingCats: true });
+    setLoading(true);
     fetch(
       `${baseUrl}/images/search?limit=${limit}&category_ids=${
         selectedCategory ? selectedCategory.value : ""
       }&mime_types=${imageTypes.join(",")}`
     )
       .then(response => response.json())
-      .then(cats => this.setState({ cats, loadingCats: false }));
-  }
+      .then(cats => {
+        setCats(cats);
+        setLoading(false);
+      });
+  }, [setLoading, setCats, selectedCategory, selectedImageTypes]);
 
-  toggle(id) {
-    let newState = this.state.selectedCats;
-    if (newState.includes(id)) {
-      newState = newState.filter(i => id !== i);
-    } else {
-      newState.push(id);
-    }
+  return (
+    <div className="SearchCatForm">
+      <Select
+        options={categories}
+        placeholder="Category"
+        value={selectedCategory}
+        onChange={v => setSelectedCategory(v)}
+        className="SearchCatForm-select"
+      />
+      <Select
+        isMulti
+        options={imageTypes}
+        placeholder="Image type"
+        value={selectedImageTypes}
+        onChange={t => setSelectedTimageTypes(t)}
+        className="SearchCatForm-select"
+      />
+      {loading ? (
+        <span>Loading cats...</span>
+      ) : (
+        <div className="SearchCatForm-results">
+          {cats.map(cat => (
+            <img
+              alt=""
+              src={cat.url}
+              onClick={() =>
+                !excludedCats.includes(cat.url) &&
+                setSelectedCats(toggle(selectedCats, cat.id))
+              }
+              key={cat.id}
+              className={`${
+                selectedCats.includes(cat.id) ? "SearchCatForm-selected" : ""
+              }${
+                excludedCats.includes(cat.url) ? "SearchCatForm-excluded" : ""
+              }`}
+            />
+          ))}
+        </div>
+      )}
+      {selectedCats.length > 0 && (
+        <button
+          className="SearchCatForm-submit"
+          onClick={() => {
+            const urls = cats
+              .filter(cat => selectedCats.includes(cat.id))
+              .map(cat => cat.url);
+            addCats(urls);
+          }}
+        >
+          Add Cats
+        </button>
+      )}
+    </div>
+  );
+}
 
-    this.setState({ selectedCats: newState });
-  }
-
-  render() {
-    const {
-      cats,
-      allCategories,
-      loadingCats,
-      selectedCats,
-      allImageTypes,
-      selectedCategory,
-      selectedImageTypes
-    } = this.state;
-
-    const { excludedCats } = this.props;
-
-    return (
-      <div className="SearchCatForm">
-        <Select
-          options={allCategories}
-          placeholder="Category"
-          value={selectedCategory}
-          onChange={selectedCategory =>
-            this.setState({ selectedCategory }, this.searchCat)
-          }
-          className="SearchCatForm-select"
-        />
-        <Select
-          isMulti
-          options={allImageTypes}
-          placeholder="Image type"
-          value={selectedImageTypes}
-          onChange={selectedImageTypes =>
-            this.setState({ selectedImageTypes }, this.searchCat)
-          }
-          className="SearchCatForm-select"
-        />
-        {loadingCats ? (
-          <span>Loading cats...</span>
-        ) : (
-          <div className="SearchCatForm-results">
-            {cats.map(cat => (
-              <img
-                alt=""
-                src={cat.url}
-                onClick={() =>
-                  !excludedCats.includes(cat.url) && this.toggle(cat.id)
-                }
-                key={cat.id}
-                className={`${
-                  selectedCats.includes(cat.id) ? "SearchCatForm-selected" : ""
-                }${
-                  excludedCats.includes(cat.url) ? "SearchCatForm-excluded" : ""
-                }`}
-              />
-            ))}
-          </div>
-        )}
-        {selectedCats.length > 0 && (
-          <button
-            className="SearchCatForm-submit"
-            onClick={() => {
-              const urls = cats
-                .filter(cat => selectedCats.includes(cat.id))
-                .map(cat => cat.url);
-              this.props.addCats(urls);
-            }}
-          >
-            Add Cats
-          </button>
-        )}
-      </div>
-    );
-  }
+function toggle(array, value) {
+  return array.includes(value)
+    ? [...array, value]
+    : array.filter(v => v !== value);
 }
 
 export default SearchCatForm;
