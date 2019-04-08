14698b895e508e0495c67251dff497dcbe6e0b58
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-04-08 08:34:35 +0200

    :rotating_light: Use Prettier

--- src/components/SearchCat/SearchCat.jsx
+++ src/components/SearchCat/SearchCat.jsx
@@ -1,11 +1,11 @@
-import React, { Fragment, useState } from "react";
-import "./SearchCat.css";
-import "./SearchCatForm";
-import SearchCatForm from "./SearchCatForm";
+import React, {Fragment, useState} from 'react';
+import './SearchCat.css';
+import './SearchCatForm';
+import SearchCatForm from './SearchCatForm';
 
 function SearchCat(props) {
-  const [displayed, setDisplayed] = useState(false)
-  const toggleForm = () => setDisplayed(!displayed)
+  const [displayed, setDisplayed] = useState(false);
+  const toggleForm = () => setDisplayed(!displayed);
 
   return (
     <Fragment>
@@ -14,10 +14,7 @@ function SearchCat(props) {
       </button>
       {displayed && (
         <div className="SearchCat-overlay" onClick={toggleForm}>
-          <div
-            className="SearchCat-content"
-            onClick={e => e.stopPropagation()}
-          >
+          <div className="SearchCat-content" onClick={e => e.stopPropagation()}>
             <SearchCatForm
               limit={100}
               excludedCats={props.excludedCats}
@@ -30,7 +27,7 @@ function SearchCat(props) {
         </div>
       )}
     </Fragment>
-  )
+  );
 }
 
 export default SearchCat;
