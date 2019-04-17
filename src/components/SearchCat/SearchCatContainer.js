8f2ead139521c9f7f59b917b919e61f67e251ec0
Benjamin Cavy <benjamin.cavy@gmail.com>
2019-04-17 14:18:14 +0200

    :art: useReducer for context

--- src/components/SearchCat/SearchCatContainer.js
+++ src/components/SearchCat/SearchCatContainer.js
@@ -6,7 +6,11 @@ class SearchCatContainer extends Component {
   static contextType = CatContext;
   render() {
     const excludedCats = this.context.cats.map(c => c.url);
-    const addCats = urls => this.context.addCats(urls.map(url => ({ url })));
+    const addCats = urls =>
+      this.context.dispatch({
+        type: "ADD_CATS",
+        cats: urls.map(url => ({ url }))
+      });
     return (
       <SearchCat
         excludedCats={excludedCats}
